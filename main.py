f25917e24d3b6b678cbf4ed15c57aabee23e97f9
Mauricio <juanpablobouzas@gmail.com>
2020-01-09 21:04:32 -0400

    wtf i forgot main.py for the audio device setting

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from PyQt4 import QtCore, QtGui
 from os import path
 from pybass import *
-import ctypes
+import ctypes, ini, sys
 
 from AIOApplication import AIOApplication
-
-import sys
 
 if not path.exists("data"):
 	ctypes.windll.user32.MessageBoxA(0, "the folder named \"data\" could not be found.\ndid you extract all the game files properly? the folder \"data\" must be situated in the same folder as the game.", "unable to launch game", 0) #if you're using linux, then i'm sorry, but not yet. just use wine for the time being
 	sys.exit(1)
 
-if not BASS_Init(-1, 44100, 0, 0, 0):
+device = ini.read_ini_int("aaio.ini", "Audio", "Device", -1)
+
+if not BASS_Init(device, 44100, 0, 0, 0):
 	sys.exit(1)
 
 app = AIOApplication(sys.argv)

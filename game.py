33996962d30ba52eb08b113f497e78a6efb4fde8
Mauricio <juanpablobouzas@gmail.com>
2020-06-10 19:38:43 -0400

    background: prioritize .png over .gif

--- game.py
+++ game.py
@@ -761,9 +761,14 @@ class GamePort(QtGui.QWidget):
 				char.chatbubblepix.hide()
 	
 	def setBackground(self, bg):
-		print "[client]", "set zone background to \"%s\", exists: %s, %s, full path: %s" % (bg, os.path.exists(bg), os.path.exists(os.path.abspath(bg+"../")), os.path.abspath(bg))
-		self.img = QtGui.QImage(bg)
-		print "[client]", "is zone QImage null:", self.img.isNull()
+		if os.path.exists(bg+".png"):
+			self.img = QtGui.QImage(bg+".png")
+			print "[client]", "load png background \"%s\", exists: %s" % (bg+".png", os.path.exists(bg+".png"))
+		elif os.path.exists(bg+".gif"):
+			self.img = QtGui.QImage(bg+".gif")
+			print "[client]", "load gif background \"%s\", exists: %s" % (bg+".gif", os.path.exists(bg+".gif"))
+
+		print "[client]", "background QImage is null: %s" % self.img.isNull()
 		self.zonebackground.setPixmap(QtGui.QPixmap.fromImage(self.img.scaled(self.img.width()*2, self.img.height()*2)))
 
 class GameWidget(QtGui.QWidget):
@@ -1842,7 +1847,7 @@ class GameWidget(QtGui.QWidget):
 		
 		self.player.zone = ind
 		zone = "data/zones/"+self.ao_app.zonelist[ind][0]
-		self.gameview.setBackground(zone+".gif")
+		self.gameview.setBackground(zone)
 		
 		for examine in self.examines:
 			self.gameview.gamescene.removeItem(examine)

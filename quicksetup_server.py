2ee2b7835c992c75be7bd8f8bfb67f13a7cebc34
headshot2017 <headshot2017@github.com>
2021-01-20 15:52:40 +0000

    pyinstaller==3.6

--- quicksetup_server.py
+++ quicksetup_server.py
@@ -8,7 +8,7 @@ def pip_install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
 print "installing pyinstaller"
-pip_install('pyinstaller')
+pip_install('pyinstaller==3.6')
 
 print "installing ini"
 pip_install('ini')

5528594216ddbfb8c88723dfec17c48e2ca49cb7
headshot2017 <headshot2017@github.com>
2021-01-20 15:50:54 +0000

    add quicksetup_server.py for server dependencies only

---
+++ quicksetup_server.py
@@ -0,0 +1,20 @@
+import urllib2
+import zipfile
+import subprocess
+import sys
+import os
+
+def pip_install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+print "installing pyinstaller"
+pip_install('pyinstaller')
+
+print "installing ini"
+pip_install('ini')
+
+print "installing requests"
+pip_install('requests')
+import requests
+
+print "done"

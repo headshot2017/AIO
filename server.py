45f4e83128b2d6aac141f564d1a695e3a656de8f
Headshotnoby <nou@is>
2021-08-30 20:56:22 -0400

    same thing but for crashes...

--- server.py
+++ server.py
@@ -1834,10 +1834,11 @@ if __name__ == "__main__":
         if server.econ_password and server.econ_tcp: server.econ_tcp.close()
         
         for plug in server.plugins:
-            server.Print("plugins", "stopping '%s' version %s" % (plug[2], plug[3].version))
-            super(plug[0], plug[1]).onPluginStop(server, True)
-            if hasattr(plug[1], "onPluginStop"):
-                plug[1].onPluginStop(server, True)
+            if plug[1].running:
+                server.Print("plugins", "stopping '%s' version %s" % (plug[2], plug[3].version))
+                super(plug[0], plug[1]).onPluginStop(server, True)
+                if hasattr(plug[1], "onPluginStop"):
+                    plug[1].onPluginStop(server, True)
         
         server.Print("server", "Server stopped due to a crash.")
 

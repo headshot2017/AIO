e1f0f5ca94ab95f43bd25ee3c6bfa26aa00f0896
headshot2017 <>
2022-02-19 22:16:14 -0400

    fix some stuff in vmath & add mix, round_to_int

--- vmath.py
+++ vmath.py
@@ -1,62 +1,50 @@
 # vectorial math stuff
-# borrowed from tw source code
+# borrowed from teeworlds source code
 
 import math
 
 class vec2:
     def __init__(self, x=0, y=0):
-        self.x = float(x)
-        self.y = float(y)
+        if isinstance(x, vec2):
+            self.x = x.x
+            self.y = x.y
+        else:
+            self.x = float(x)
+            self.y = float(y)
 
     def __sub__(self, other):
         if isinstance(other, vec2):
-            self.x -= other.x
-            self.y -= other.y
-        else:
-            self.x -= other
-            self.y -= other
-        return self
+            return vec2(self.x-other.x, self.y-other.y)
+        return vec2(self.x-other, self.y-other)
 
     def __add__(self, other):
         if isinstance(other, vec2):
-            self.x += other.x
-            self.y += other.y
-        else:
-            self.x += other
-            self.y += other
-        return self
+            return vec2(self.x+other.x, self.y+other.y)
+        return vec2(self.x+other, self.y+other)
 
     def __mul__(self, other):
         if isinstance(other, vec2):
-            self.x *= other.x
-            self.y *= other.y
-        else:
-            self.x *= other
-            self.y *= other
-        return self
+            return vec2(self.x*other.x, self.y*other.y)
+        return vec2(self.x*other, self.y*other)
 
     def __truediv__(self, other):
         if isinstance(other, vec2):
-            self.x /= other.x
-            self.y /= other.y
-        else:
-            self.x /= other
-            self.y /= other
-        return self
+            return vec2(self.x/other.x, self.y/other.y)
+        return vec2(self.x/other, self.y/other)
 
     def __floordiv__(self, other):
         if isinstance(other, vec2):
-            self.x //= other.x
-            self.y //= other.y
-        else:
-            self.x //= other
-            self.y //= other
-        return self
+            return vec2(self.x//other.x, self.y//other.y)
+        return vec2(self.x//other, self.y//other)
+
+    def __neg__(self):
+        return vec2(-self.x, -self.y)
 
     def __repr__(self):
-        return "vmath.vec2(%d,%d)"%(self.x,self.y)
+        return "vmath.vec2(%.3f,%.3f)"%(self.x,self.y)
 
-def length(a):
+
+def length(a): # also magnitude
 	return math.sqrt(a.x*a.x + a.y*a.y)
 
 def distance(a, b):
@@ -65,6 +53,14 @@ def distance(a, b):
 def dot(a, b):
 	return a.x*b.x + a.y*b.y
 
+def mix(a, b, amount):
+    return a + (b - a) * amount
+
+def round_to_int(f):
+	return int(f + 0.5) if f > 0 else int(f - 0.5)
+
 def normalize(v):
-	l = (1./math.sqrt(v.x*v.x + v.y*v.y))
+	div = math.sqrt(v.x*v.x + v.y*v.y)
+	if div == 0: return vec2(0,0)
+	l = (1./div)
 	return vec2(v.x*l, v.y*l)
